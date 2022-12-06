7d1115b57eb6d0be842ef99557bbf0af2b329758
Dhallus~ <homes86451@gmail.com>
2022-12-06 10:45:34 -0800

    project finished

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -22,7 +22,7 @@ const Contact = () => {
 
         <div className=" flex justify-center items-center">
           <form
-            action="https://getform.io/f/61c99527-2b15-42cf-9b55-ad37d2f7daa6"
+            action="https://getform.io/f/1dac5b18-414c-4dbe-868d-9020b21c1794"
             method="POST"
             className=" flex flex-col w-full md:w-1/2"
           >
